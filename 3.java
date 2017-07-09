004ae69fd1bfb6b1f963931456ca2431bc115e10
nixgeo97 <nixgeo97@gmail.com>
2017-07-09 16:36:53 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -13,7 +13,7 @@ class Ideone
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter alphabet");
 		x=s.next().charAt(0);
-		if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U');
+		if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
 		{
 		System.out.println("Vowels");
 		}
